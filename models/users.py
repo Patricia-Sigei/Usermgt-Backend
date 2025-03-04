b5470119cf0d9a83776b885d1baea840a504b0f3
Patricia-Sigei <pattychela97@gmail.com>
2025-03-04 09:10:08 +0300

    removes the scanned relationship

--- models/users.py
+++ models/users.py
@@ -5,14 +5,13 @@ class User(db.Model):
     __tablename__ = "user"  
 
     id = db.Column(db.Integer, primary_key=True)
-    scanned_id =db.Column(db.Integer, db.ForeignKey('scanned.id'), nullable=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), unique=True, nullable=False)
     password= db.Column(db.String(120), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
 
-    scanned = db.relationship("Scanned", back_populates="user")
+   
 
     role = db.relationship("Role", back_populates="users")  
 
@@ -26,5 +25,4 @@ class User(db.Model):
             "email": self.email,
             "phone_number": self.phone_number,
             "role": self.role.name if self.role else None,
-            "scanned": [scanned.to_dict()for scanned in self.scanned]
         }
