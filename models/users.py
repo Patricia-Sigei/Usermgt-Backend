469d4774b92e2248ccbea36147c26ba27ec13ada
Patricia-Sigei <pattychela97@gmail.com>
2025-03-04 08:50:55 +0300

    adds relationship to scanned

--- models/users.py
+++ models/users.py
@@ -11,6 +11,8 @@ class User(db.Model):
     password= db.Column(db.String(120), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
 
+    scanned = db.relationship("Scanned", back_populates="user")
+
     role = db.relationship("Role", back_populates="users")  
 
     def __repr__(self):
@@ -22,5 +24,6 @@ class User(db.Model):
             "name": self.name,
             "email": self.email,
             "phone_number": self.phone_number,
-            "role": self.role.name if self.role else None
+            "role": self.role.name if self.role else None,
+            "scanned": [scanned.to_dict()for scanned in self.scanned]
         }
